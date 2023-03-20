f92fddc35c5a7791535f8319a458b2fcc4e3d942
sipos-szabo.denes <denessiposszabo@gmail.com>
2023-03-20 15:42:54 +0100

    a

--- ujprog/1.py
+++ ujprog/1.py
@@ -1,9 +1,10 @@
+
+
 nevek = []
 magassagok = []
 nev = input("Adja meg a nevet")
 while nev!="vege":
     magassag = input("Mekkora az ember?")
-
     while magassag.isnumeric()!=True:
         print("nem jó. Ujra")
         magassag = input("Mekkora az ember?")
@@ -11,3 +12,9 @@ while nev!="vege":
     nevek.append(nev)
     magassagok.append(magassag)
     nev = input("adja meg a nevet")
+
+print("Nev Magassag")
+for i in range(len(nevek)):
+    print(nevek[i],magassagok[i])
+
+print("Átlag", sum(magassagok)/len(magassagok))
